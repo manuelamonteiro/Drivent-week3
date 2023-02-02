09504e98e9680baee69b84e4205dc6d6af2c2cae
manuelamp <manuella.m.p182@gmail.com>
2023-02-02 17:44:13 -0300

    feat: hotel, room and ticket factory and basic hotel test

---
+++ tests/integration/hotels.test.ts
@@ -0,0 +1,26 @@
+import app, { init } from "@/app";
+import { prisma } from "@/config";
+import faker from "@faker-js/faker";
+import { TicketStatus } from "@prisma/client";
+import httpStatus from "http-status";
+import * as jwt from "jsonwebtoken";
+import supertest from "supertest";
+import {
+    createHotel,
+    createRoom,
+    createUser,
+    createEnrollmentWithAddress,
+    createTicketType,
+    createTicket
+} from "../factories";
+import { cleanDb, generateValidToken } from "../helpers";
+
+beforeAll(async () => {
+    await init();
+});
+
+beforeEach(async () => {
+    await cleanDb();
+});
+
+const server = supertest(app);
