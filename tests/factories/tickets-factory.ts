68ed436a927dee5c4026614e2fea2a77c7bd23b8
manuelamp <manuella.m.p182@gmail.com>
2023-02-04 10:52:44 -0300

    feat: ticket factory fixed and auth test

--- tests/factories/tickets-factory.ts
+++ tests/factories/tickets-factory.ts
@@ -2,13 +2,13 @@ import faker from "@faker-js/faker";
 import { prisma } from "@/config";
 import { TicketStatus } from "@prisma/client";
 
-export async function createTicketType(remote: boolean, includesHotel: boolean) {
+export async function createTicketType() {
   return prisma.ticketType.create({
     data: {
       name: faker.name.findName(),
       price: faker.datatype.number(),
-      isRemote: remote,
-      includesHotel: includesHotel,
+      isRemote: faker.datatype.boolean(),
+      includesHotel: faker.datatype.boolean(),
     },
   });
 }
@@ -19,6 +19,28 @@ export async function createTicket(enrollmentId: number, ticketTypeId: number, s
       enrollmentId,
       ticketTypeId,
       status,
+    },
+  });
+}
+
+export async function createTicketTypeWithoutHotel() {
+  return prisma.ticketType.create({
+    data: {
+      name: faker.name.findName(),
+      price: faker.datatype.number(),
+      isRemote: true,
+      includesHotel: false,
+    },
+  });
+}
+
+export async function createTicketTypeWithHotel() {
+  return prisma.ticketType.create({
+    data: {
+      name: faker.name.findName(),
+      price: faker.datatype.number(),
+      isRemote: false,
+      includesHotel: true,
     },
   });
 }
